1c9f3d7424602c8409135e4de93ac54d22af1e05
Ankit Shaw <ankittshaw05@gmail.com>
2026-09-03 21:02:20 +0530

    Time: 8 ms (50.00%) | Memory: 121.6 MB (62.32%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int smallOdd = Integer.MAX_VALUE;
+        for(int num : nums1){
+            if(num %2 ==1)
+            smallOdd = Math.min(smallOdd,num);
+        }
+        if(smallOdd == Integer.MAX_VALUE)
+        return true;
+
+        for(int num : nums1){
+            if(num % 2 == 0 && num<= smallOdd)
+            return false;
+        }
+        return true;
+    }
+}
